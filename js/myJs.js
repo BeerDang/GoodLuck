cc4061035c325ada32953a11998aa2e409b8dc06
BeerDang <86824409+BeerDang@users.noreply.github.com>
2021-07-02 14:43:36 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -105,7 +105,7 @@ $('#yes').click(function() {
         backdrop: `
               rgba(0,0,123,0.4)
               url("img/giphy2.gif")
-              middle top
+              left top
               no-repeat
             `,
   
